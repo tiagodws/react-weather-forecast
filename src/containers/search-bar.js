758635f29e5c2644a57d20d3d1e8102f93b2da49
Tiago Dionesto Willrich da Silva <tiagodws@gmail.com>
2018-02-13 02:14:54 -0200

    search placeholder fix

--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -31,7 +31,7 @@ class SearchBar extends Component {
         return (
             <form className="input-group form-group" onSubmit={this.onFormSubmit}>
                 <input
-                    placeholder="Get a five-day forecast in your favorite cities"
+                    placeholder="Get a five-day forecast in your favorite Brazilian cities"
                     className="form-control"
                     value={this.state.term}
                     onChange={this.onInputChange}
